9a7746935ffd5743581ed532ecc71756de080ab0
om65praj <om65praj@gmail.com>
2017-09-13 19:23:15 +0530

    commit

--- src/com/bridgelabz/Programs/RegularExpression.java
+++ src/com/bridgelabz/Programs/RegularExpression.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.Programs;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+
 import com.bridgelabz.Utility.Utility;
 
 /**
@@ -9,7 +14,31 @@ import com.bridgelabz.Utility.Utility;
 public class RegularExpression {
 
 	public static void main(String[] args) {
+		
 		Utility utility = new Utility();
-		utility.regularExpression();
+			File file = new File("/home/bridgelabz/Public/file/regularExpression.txt");
+			FileReader filereader;
+			try {
+				filereader = new FileReader(file);
+				int length = (int) file.length();
+				char[] charArray = new char[length];
+				filereader.read(charArray);
+				String string = new String(charArray);
+				System.out.print("Enter your first name: ");
+				String firstName = utility.inputString();
+				System.out.print("Enter your full name: ");
+				String fullName = utility.inputString();
+				System.out.print("Enter your contact number: ");
+				String contactNumber = utility.inputString();
+				System.out.print("Enter date in dd/mm/yyyy format: ");
+				String date = utility.inputString();
+				string = string.replaceAll("<<name>>", firstName);
+				string = string.replaceAll("<<full name>>", fullName);
+				string = string.replaceAll("xxxxxxxxxx", contactNumber);
+				string = string.replaceAll("01/01/2016", date);
+				System.out.println(string);
+			} catch (FileSystemNotFoundException | IOException e) {
+				e.printStackTrace();
+			}
 	}
 }
--- src/com/bridgelabz/Utility/Utility.java
+++ src/com/bridgelabz/Utility/Utility.java
@@ -89,39 +89,6 @@ public class Utility {
 
 
 	/**
-	 * @param create
-	 *            method of regular Expression
-	 * @throws IOException
-	 * 
-	 */
-	public void regularExpression() {
-		File file = new File("/home/bridgelabz/Public/file/regularExpression.txt");
-		FileReader filereader;
-		try {
-			filereader = new FileReader(file);
-			int length = (int) file.length();
-			char[] charArray = new char[length];
-			filereader.read(charArray);
-			String string = new String(charArray);
-			System.out.print("Enter your first name: ");
-			String firstName = scanner.nextLine();
-			System.out.print("Enter your full name: ");
-			String fullName = scanner.nextLine();
-			System.out.print("Enter your contact number: ");
-			String contactNumber = scanner.nextLine();
-			System.out.print("Enter date in dd/mm/yyyy format: ");
-			String date = scanner.nextLine();
-			string = string.replaceAll("<<name>>", firstName);
-			string = string.replaceAll("<<full name>>", fullName);
-			string = string.replaceAll("xxxxxxxxxx", contactNumber);
-			string = string.replaceAll("01/01/2016", date);
-			System.out.println(string);
-		} catch (FileSystemNotFoundException | IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
 	 * create a method to sort a string array
 	 * 
 	 */
